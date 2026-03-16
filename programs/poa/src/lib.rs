52a4857f02c93d09b29a0c117c3fae86a308a0fd
KiyoKanshiEdits <kiyokanshicrypto@gmail.com>
2026-03-16 18:43:11 +0000

    Update lib.rs

--- programs/poa/src/lib.rs
+++ programs/poa/src/lib.rs
@@ -10,6 +10,7 @@ pub mod poa {
 
     pub fn issue_receipt(
         ctx: Context<IssueReceipt>,
+        receipt_id: [u8; 16],
         model_hash: [u8; 32],
         input_hash: [u8; 32],
         output_hash: [u8; 32],
@@ -19,21 +20,23 @@ pub mod poa {
         let slot = Clock::get()?.slot;
 
         receipt.agent = agent;
+        receipt.receipt_id = receipt_id;
         receipt.model_hash = model_hash;
         receipt.input_hash = input_hash;
         receipt.output_hash = output_hash;
         receipt.slot = slot;
         receipt.is_valid = true;
 
-        let mut data = Vec::with_capacity(136);
+        let mut data = Vec::with_capacity(152);
         data.extend_from_slice(agent.as_ref());
+        data.extend_from_slice(&receipt_id);
         data.extend_from_slice(&model_hash);
         data.extend_from_slice(&input_hash);
         data.extend_from_slice(&output_hash);
         data.extend_from_slice(&slot.to_le_bytes());
         receipt.receipt_hash = solana_program::hash::hash(&data).to_bytes();
-        emit!(ReceiptIssued {
 
+        emit!(ReceiptIssued {
             receipt_hash: receipt.receipt_hash,
             agent,
             model_hash,
@@ -47,8 +50,9 @@ pub mod poa {
     pub fn verify_receipt(ctx: Context<VerifyReceipt>) -> Result<bool> {
         let receipt = &ctx.accounts.receipt;
 
-        let mut data = Vec::with_capacity(136);
+        let mut data = Vec::with_capacity(152);
         data.extend_from_slice(receipt.agent.as_ref());
+        data.extend_from_slice(&receipt.receipt_id);
         data.extend_from_slice(&receipt.model_hash);
         data.extend_from_slice(&receipt.input_hash);
         data.extend_from_slice(&receipt.output_hash);
@@ -79,6 +83,7 @@ pub mod poa {
 #[derive(InitSpace)]
 pub struct ComputeReceipt {
     pub agent: Pubkey,
+    pub receipt_id: [u8; 16],
     pub model_hash: [u8; 32],
     pub input_hash: [u8; 32],
     pub output_hash: [u8; 32],
@@ -88,7 +93,7 @@ pub struct ComputeReceipt {
 }
 
 #[derive(Accounts)]
-#[instruction(model_hash: [u8; 32], input_hash: [u8; 32], output_hash: [u8; 32])]
+#[instruction(receipt_id: [u8; 16], model_hash: [u8; 32], input_hash: [u8; 32], output_hash: [u8; 32])]
 pub struct IssueReceipt<'info> {
     #[account(mut)]
     pub agent: Signer<'info>,
@@ -100,21 +105,10 @@ pub struct IssueReceipt<'info> {
         seeds = [
             b"receipt",
             agent.key().as_ref(),
-            input_hash.as_ref(),
+            receipt_id.as_ref(),
         ],
         bump,
     )]
-
-
-
-
-
-
-
-
-
-
-
     pub receipt: Account<'info, ComputeReceipt>,
 
     pub system_program: Program<'info, System>,
