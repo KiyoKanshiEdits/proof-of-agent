8ee608fca7bf2eef18414b0bc20084f18cb9c972
proofofagent <12345678+KiyoKanshiEdits@users.noreply.github.com>
2026-03-16 19:21:19 +0000

    chore: remove sdk/dist from tracking

--- sdk/dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { PoA } from "./poa";
-export type { PoAConfig, IssueReceiptParams, Receipt, IssueResult, } from "./poa";
-//# sourceMappingURL=index.d.ts.map
--- sdk/dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PoA = void 0;
-var poa_1 = require("./poa");
-Object.defineProperty(exports, "PoA", { enumerable: true, get: function () { return poa_1.PoA; } });
-//# sourceMappingURL=index.js.map
